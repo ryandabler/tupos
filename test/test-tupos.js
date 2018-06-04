a5adf05676fe190445bca466f73f7a5f6585d57a
Ryan Dabler <rpdabler@gmail.com>
2018-06-04 14:32:13 -0600

    add test suite

---
+++ test/test-tupos.js
@@ -0,0 +1,66 @@
+////////////////////////////
+// Initialize
+////////////////////////////
+const chai  = require("chai");
+const {
+    typeOf,
+    isIterable
+} = require("../src/tupos");
+const types = require("../src/constants");
+
+const expect = chai.expect;
+
+////////////////////////////
+// Test
+////////////////////////////
+describe("typeOf()", function() {
+    it("Should return type of supplied parameter", function() {
+        const typesToTest = [
+            {a: 1},
+            [1, 2, 3],
+            1,
+            "Hello",
+            () => 1
+        ];
+        const results = typesToTest.map(typeOf);
+        const answers = [
+            types.OBJECT,
+            types.ARRAY,
+            types.NUMBER,
+            types.STRING,
+            types.FUNCTION
+        ];
+
+        results.forEach((result, idx) => {
+            expect(result).to.equal(answers[idx]);
+        });
+    });
+});
+
+describe("isIterable()", function() {
+    it("Should return true", function() {
+        const objectsToTest = [
+            [1, 2, 3],
+            new Map(),
+            "Hello",
+        ];
+        const results = objectsToTest.map(isIterable);
+
+        results.forEach(result => {
+            expect(result).to.be.true;
+        });
+    });
+
+    it("Should return false", function() {
+        const objectsToTest = [
+            1,
+            {a: 1},
+            () => {},
+        ];
+        const results = objectsToTest.map(isIterable);
+
+        results.forEach(result => {
+            expect(result).to.be.false;
+        });
+    });
+});
