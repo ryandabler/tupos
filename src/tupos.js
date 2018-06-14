fba38ac33867e556697432728f7b992142101f0f
Ryan Dabler <rpdabler@gmail.com>
2018-06-13 19:43:18 -0600

    add documentation for isPrimitive()

--- src/tupos.js
+++ src/tupos.js
@@ -25,6 +25,11 @@ const typeOf = obj =>
 const isIterable = obj =>
     obj ? typeOf(obj[Symbol.iterator]) === types.FUNCTION : false
 
+/**
+ * Determines whether supplied param is a primitive type
+ * @param {*} param 
+ * @returns {boolean}
+ */
 const isPrimitive = param => [
         types.NUMBER,
         types.STRING,
